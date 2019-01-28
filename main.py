c7b529e89492fb2eb63c7be27d52c349b25b24fd
Raul Vences <raul.vences@p-i.net>
2019-01-28 13:41:08 +0000

    stablishes trials

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+import time
+
 
 
 #TODO: Train Randon forest classifiers
@@ -91,7 +93,7 @@ clf.set_params(**initial_params)
 print(cross_val_score(clf, X_train, y_train, cv=7))
 
 
-def the_objective(params, X_train_s, y_train_s, X_test, y_test, cv = 3):
+def the_objective(params, X_train_s = X_train,y_train_s = y_train,X_test = X_test, y_test = y_test):
        '''
        :param params: Random Forest Params
        :param cv: Cross Validation folds
@@ -100,30 +102,90 @@ def the_objective(params, X_train_s, y_train_s, X_test, y_test, cv = 3):
 
        clf_ =  RandomForestClassifier()
        clf_.set_params(**params)
+       print("Trying the set of paras {}".format(params))
        clf_.fit(X_train_s,y_train_s)
        y_pred = clf_.predict(X_test)
        accuracy_score =  metrics.accuracy_score(y_test, y_pred)
+       print("The accuracy score is {}".format(accuracy_score))
+
        accuracy_inverse = 1 - accuracy_score
        #best = cv_results.mean()
        #print(cv_results)
-       return accuracy_score
+       return {'loss': accuracy_inverse, 'params': params, 'status': STATUS_OK, 'accuracy': accuracy_score}
 
 #define space
+'''
+Using this set of hyperparams
+
+n_estimators = number of trees in the foreset
+max_features = max number of features considered for splitting a node
+max_depth = max number of levels in each decision tree
+min_samples_split = min number of data points placed in a node before the node is split
+min_samples_leaf = min number of data points allowed in a leaf node
+bootstrap = method for sampling data points (with or without replacement)
+
+Taking the Bayesian Strategy to find the best combination
+'''
+
+import numpy as np
+
+grid_space = {'bootstrap': [True, False],
+ 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+ 'min_samples_leaf': [1, 2, 4],
+ 'min_samples_split': [2, 5, 10],
+ 'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]}
+
+
+bayes_space = {'bootstrap': hp.choice('bootstrap',[True, False]),
+ 'max_depth': hp.choice('max_depth', np.arange(3,30, dtype= int)),
+ 'min_samples_leaf': hp.choice('min_samples_leaf', np.arange(3,60, dtype= int)),
+ 'min_samples_split': hp.choice('min_samples_split', np.arange(3,100, dtype= int)),
+ 'n_estimators': hp.choice('n_estimators', np.arange(200,2000, dtype= int))
+               }
+
+
+#testing space
+from hyperopt.pyll.stochastic import sample
+print('Testing Space')
+print(sample(bayes_space))
+
 #run tunning
-#get results for best
+
+from hyperopt import Trials, tpe, fmin
 
 
-the_objective(params = initial_params, X_train_s = X_train,y_train_s = y_train,X_test = X_test, y_test = y_test,  cv = 6)
+bayes_trials = Trials()
 
+best = fmin(fn = the_objective, space = bayes_space, algo = tpe.suggest, max_evals = 50, trials= bayes_trials)
 
-#TODO: Train Logistic Regression
+print(best)
 
-#TODO: Compare Feature Performances
+#print
+#unnesting the parameters to columns
+#Getting all results
+df_best = pd.DataFrame(bayes_trials.results)
 
-#TODO: Create a Pipeline at least one class
+#unnesting the params dictionary
+df_best = pd.concat([df_best, df_best.params.apply(pd.Series)], axis = 1, sort = False)
+#dropping the dictionary column
+df_best = df_best.drop(['params'], axis = 1)
+print(df_best.head())
 
-#TODO: Include bag of words
+df_best['iteration'] = df_best.index + 1
 
+print(df_best.columns)
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+print_list = ['accuracy', 'loss', 'max_depth',
+       'min_samples_leaf', 'min_samples_split', 'n_estimators', 'iteration']
+
+fig, ax = plt.subplots()
+
+for i,item in enumerate(print_list):
+       sns.regplot('iteration', item, data = df_best)
+       plt.show()
+
+#train model with best hyperparameters
 
